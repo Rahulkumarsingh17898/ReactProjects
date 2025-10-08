9bd6ce668903ddb06c463dcc0bd5accd9cb6542a
rahulkumarsingh <rahulkumarsingh17898@gmail.com>
2025-10-08 18:44:27 +0530

    initial project

--- README.md
+++
@@ -1,1 +0,0 @@
-The main branch has Tic Tac Toe With React fundamental understanding of important concepts
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import Player from './components/Player.jsx';
-import TimerChallenge from './components/TimerChallenge.jsx';
-
 function App() {
   return (
-    <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title="Easy Peasy" targetTime={1} />
-        <TimerChallenge title="Not Easy" targetTime={5} />
-        <TimerChallenge title="Getting Rough" targetTime={10} />
-        <TimerChallenge title="Pros Only" targetTime={15} />
-      </div>
-    </>
-  );
+    <h1>React Investment Calculator</h1>
+  )
 }
 
-export default App;
+export default App
